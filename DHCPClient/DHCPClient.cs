49bd021cd78836e4a394c2693b8e3134024d30c1
Thái Phạm Thành <19520938@ms.uit.edu.vn>
2021-05-05 09:46:50 +0700

    DHCP Client

--- DHCPClient/DHCPClient.cs
+++ DHCPClient/DHCPClient.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace DHCPClient
+namespace DHCPStimulate
 {
-    public partial class Form1 : Form
+    public partial class DHCPClient : Form
     {
-        public Form1()
+        public DHCPClient()
         {
             InitializeComponent();
         }
